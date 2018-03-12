817dc8d5e94d6b390fd1bd27c66fc66c711c1072
OyvindSabo <oyvind.sabo@gmail.com>
2018-03-12 07:16:38 +0100

    Added the whole unfinished project to the git folder lol.

---
+++ core/src/com/oyvindsabo/multiplayerdemo/MultiplayerDemo.java
@@ -0,0 +1,73 @@
+package com.oyvindsabo.multiplayerdemo;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import io.socket.client.IO;
+import io.socket.client.Socket;
+import io.socket.emitter.Emitter;
+
+public class MultiplayerDemo extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture img;
+	private Socket socket;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+		connectSocket();
+		configSocketEvents();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	public void connectSocket() {
+		try {
+			socket = IO.socket("http://localhost:8080");
+			socket.connect();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+		public void configSocketEvents() {
+			socket.on(Socket.EVENT_CONNECT, new Emitter.Listener() {
+				@Override
+				public void call(Object... args) {
+					Gdx.app.log("SocketIO", "Connected");
+				}
+			}).on("socketID", new Emitter.Listener() {
+				@Override
+				public void call(Object... args) {
+					JSONObject data = (JSONObject) args[0];
+					try {
+						String id = data.getString("id");
+						Gdx.app.log("SocketIO", "My ID: " + id);
+					} catch (JSONException e) {
+						Gdx.app.log("SocketIO", "Error getting ID");
+					}
+				}
+			});
+		}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		img.dispose();
+	}
+}
